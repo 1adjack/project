1636a552d65306779f77df2bfd70d4616fa58328
ladjack <1adjack@inbox.ru>
2020-02-18 23:17:04 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+a = 5
+console.log(a);
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log(4/0);
+
+
+let persone = {
+ name: "John",
+ age: 25,
+ isMarried: false
+};
+
+console.log(persone["name"]);
+
+let arr = ['plum.png','orange.jpg','apple.bmp']
+
+console.log(arr[2])
+
+alert("Hello World");
+
+//let answer = +prompt("Есть ли вам 18 лет?", "Да");
+// console.log(typeof(answer));
+
+// console.log("arr" + "- object");
+// console.log(4 + +"- object");
+
+let incr = 10,
+    decr = 10;
+
+    
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+
+console.log("2"=== 2);
+
+let isChecked = false,
+    isClose = false;
+
+    console.log(isChecked || !isClose);
+
+
